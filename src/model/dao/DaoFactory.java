f233cdce2dd1f226b0f08f28f3b3efd29804e4ac
Bryan Macedo <contato.bryansm@hotmail.com>
2025-02-25 09:18:09 -0300

    DepartmentDao implementation and test.

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,14 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJBDC;
 
 public class DaoFactory {
     public static SellerDao createdSellerDao(){
         return new SellerDaoJBDC(DB.getConnection());
+    }
+    public static DepartmentDao createdDepartmentDao(){
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
